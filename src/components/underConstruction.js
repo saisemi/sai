887c72e2e02f08c7eadb745f487ff73dbea506f1
saisemi <54761246+saisemi@users.noreply.github.com>
2019-09-01 11:39:15 +0530

    Add files via upload

---
+++ src/components/underConstruction.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from 'react-bootstrap';
+import constructionImg from '../img/construction.gif';
+
+const BodyContainer = styled(Container)`
+  margin-top: 60px;
+  margin-bottom: 60px;
+  background-image: url(${constructionImg});
+  background-position: inherit;
+  min-height: 400px;
+  font-weight: bold;
+`;
+
+
+const UnderConstruction = () => (
+    <BodyContainer>
+      <h1>UNDER CONSTRUCTION</h1>
+    </BodyContainer>
+);
+
+export default UnderConstruction;
