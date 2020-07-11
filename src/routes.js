aec20f063083982c7208c12067bc1abaab0d6b32
relaxvinodh <relaxvinodh@gmail.com>
2020-07-11 15:57:18 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
 import ContactUsPage from './components/ContactUsPage';
 import CustomersPage from './components/CustomersPage';
 import PartnersPage from './components/PartnersPage';
